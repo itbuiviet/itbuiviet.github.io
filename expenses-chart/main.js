4b4db622f1d8589ab56f0f9bc6f4d9899e292538
itbuiviet <106644014+itbuiviet@users.noreply.github.com>
2022-07-04 10:39:38 +0700

    Update main.js
    update code with json file

--- expenses-chart/main.js
+++ expenses-chart/main.js
@@ -1,48 +1,25 @@
 const column = document.querySelectorAll(".column")
 const tooltip = document.querySelectorAll(".tooltip")
+const requestFile = './data.json'
 let today = new Date()
 let Day= today.getDay()
-const data =[
-    {
-    "day": "mon",
-    "amount" : 17.45
-  },
-  {
-    "day": "tue",
-    "amount": 34.91
-  },
-  {
-    "day": "wed",
-    "amount": 52.36
-  },
-  {
-    "day": "thu",
-    "amount": 31.07
-  },
-  {
-    "day": "fri",
-    "amount": 23.39
-  },
-  {
-    "day": "sat",
-    "amount": 43.28
-  },
-  {
-    "day": "sun",
-    "amount": 25.48
-  }];
 
+fetch('./data.json')
+  .then(response => {return response.json();})
+  .then(data => {
+  
 // Change column height
 
-for (var i=0;i<7;i++){
-    column[i].style.height = `${Math.floor(data[i].amount*2.5)}px`
-    tooltip[i].textContent =`$${data[i].amount}`
-}
+  for (var i=0;i<7;i++){
+    column[i].style.height = `${Math.floor(data[i].amount*2.5)}px`;
+    tooltip[i].textContent =`$${data[i].amount}`;
+  }
 
 // Change color by day of week
- 
-if (Day===0){
-    column[6].style.backgroundColor = "hsl(186, 34%, 60%)"
-} else{
-    column[Day-1].style.backgroundColor= "hsl(186, 34%, 60%)"
-}
+
+  if (Day===0){
+    column[6].style.backgroundColor = "hsl(186, 34%, 60%)";
+  } else {
+    column[Day-1].style.backgroundColor= "hsl(186, 34%, 60%)";
+  }
+})
